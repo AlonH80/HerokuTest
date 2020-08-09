1f765bd4df7e6ec4439c4b535ebd949a310618d0
Alon Hartanu <alonhartanu@gmail.com>
2020-08-10 01:12:14 +0300

    with Procfile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            System.out.println("Starting server...c" +
+                    "");
             Server server = new Server();
         }
         catch (Exception e) {
